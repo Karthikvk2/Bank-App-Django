8fe54294a031dfd8e06ae9ebf006db8f59990de3
Karthikvk2 <vkottaryk@gmail.com>
2024-09-20 22:23:08 +0530

    final

--- BankApp/BankApp/settings.py
+++ BankApp/BankApp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'BankApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
