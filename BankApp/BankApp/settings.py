97c305106acf4adb1ed63fcccbe69824501c29ec
Karthikvk2 <vkottaryk@gmail.com>
2024-09-20 22:37:30 +0530

    final

--- BankApp/BankApp/settings.py
+++ BankApp/BankApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4fn-gy)gfl2pss!g-w&#pi@_f1ekf31r-zcu86b*x9@4nt6%cb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- BankApp/BankApp/wsgi.py
+++ BankApp/BankApp/wsgi.py
@@ -15,3 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BankApp.settings')
 
 application = get_wsgi_application()
 
+app = application
